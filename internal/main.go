9f28628eb42bba5f8b3bcf942736ceb42b392360
Victor Piolin <victor.piolin@gmail.com>
2023-10-05 21:55:25 -0400

    feat(start): handle

--- internal/main.go
+++ internal/main.go
@@ -6,11 +6,8 @@ import (
 	"os"
 	"time"
 
-	"github.com/Vico1993/Otto-bot/internal/database"
 	"github.com/Vico1993/Otto-bot/internal/handles"
 	"github.com/Vico1993/Otto-bot/internal/middleware"
-	"github.com/Vico1993/Otto-bot/internal/model"
-	"github.com/Vico1993/Otto-bot/internal/repository"
 	"github.com/subosito/gotenv"
 	tele "gopkg.in/telebot.v3"
 )
@@ -30,25 +27,13 @@ func main() {
 		return
 	}
 
-	// Model initialisation
-	model.Init()
-
-	// Load the database
-	database.Init()
-
-	// Load repositories
-	repository.Init()
-
 	// Middleware
 	b.Use(middleware.TypeCheck)
 
 	// Handles
 	b.Handle("/hello", handles.Hello)
-	b.Handle("/init", handles.Init)
 	b.Handle("/ping", handles.Ping)
-	b.Handle("/list", handles.List)
-
-	b.Handle(tele.OnText, handles.OnText)
+	b.Handle("/start", handles.Start)
 
 	fmt.Println("Ready to Go!...")
 
